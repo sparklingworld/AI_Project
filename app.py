358e2b0a9c366f1975e6c71eac9ddb4faa1fde91
Ankita_spark <169149745+sparklingworld@users.noreply.github.com>
2025-06-16 22:32:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import random
 import spacy
 from datetime import datetime
 import spacy.cli
+import os
 spacy.cli.download("en_core_web_sm") 
 nlp = spacy.load("en_core_web_sm")
 model = joblib.load("ankita_intent_model.pkl")
@@ -109,4 +110,6 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
